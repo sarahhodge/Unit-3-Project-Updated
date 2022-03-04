3ca03503f86a226d42e7a9963c573d70c1b5802b
Sarah <smkb17@gmail.com>
2022-03-04 11:02:37 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,34 +1,34 @@
 //job role option
-const focusText = document.getElementById(‘name’).focus();
-const jobInput= document.getElementById(‘other-job-role’);
-const jobTitle= document.getElementById(‘title’);
+const focusText = document.getElementById('name').focus();
+const jobInput= document.getElementById('other-job-role');
+const jobTitle= document.getElementById('title');
 
 //hiding the input initially
-jobInput.style.display= ‘none’;
+jobInput.style.display= 'none';
 
 //if they select other then the input will appear
-jobTitle.addEventListener(‘change’, (e) => {
-  if (e.target.value === ‘other’) {
-       jobInput.style.display = ‘block’;
+jobTitle.addEventListener('change', (e) => {
+  if (e.target.value === 'other') {
+       jobInput.style.display = 'block';
    }else{
-       jobInput.style.display = ‘none’;
+       jobInput.style.display = 'none';
    }
 });
 
 
 //tshirt info section
 //choose a design, then a color (color wont appear until a design is selected)
-const design = document.getElementById(‘design’);
-const color = document.getElementById(‘color’);
+const design = document.getElementById('design');
+const color = document.getElementById('color');
 const shirtColor = color.children;
-shirtColor.textContent = ‘Please select a T-shirt color’;
+shirtColor.textContent = 'Please select a T-shirt color';
 color.disabled = true;
 
-design.addEventListener(‘change’, (e) => {
+design.addEventListener('change', (e) => {
   color.disabled = false;
   for(let i = 0; i < shirtColor.length; i++){
     const theEvent = e.target.value;
-    const attribute = shirtColor[i].getAttribute(‘data-theme’);
+    const attribute = shirtColor[i].getAttribute('data-theme');
     if (attribute === theEvent) {
         shirtColor[i].setAttribute = true;
         shirtColor[i].hidden = false;
@@ -43,16 +43,16 @@ design.addEventListener(‘change’, (e) => {
 //Register for activities section
 //when selecting an activity, the totals section will add all the events selected
 //variables for activities
-const activities = document.getElementById(‘activities’);
-const activityCost = document.getElementById(‘activities-cost’);
-const checked = document.querySelectorAll(‘[type=“checkbox”]’);
-const activityBox = document.getElementById(‘activities-box’);
+const activities = document.getElementById('activities');
+const activityCost = document.getElementById('activities-cost');
+const checked = document.querySelectorAll('[type=“checkbox”]');
+const activityBox = document.getElementById('activities-box');
 let totalCost = 0;
 
 
 //Created and event to listener for the total cost of selected activities
-activities.addEventListener(‘change’, e => {
-  let eventCost = parseInt(event.target.getAttribute(‘data-cost’))
+activities.addEventListener('change', e => {
+  let eventCost = parseInt(event.target.getAttribute('data-cost'))
     if (event.target.checked){
         totalCost += eventCost;
       }
@@ -66,62 +66,62 @@ activities.addEventListener(‘change’, e => {
 
 //Payment info section
 //when you select a payment method it should hide the other options
-const payment = document.getElementById(‘payment’);
-const creditCard = document.getElementById(‘credit-card’);
-const paypal = document.getElementById(‘paypal’);
-const bitcoin = document.getElementById(‘bitcoin’);
+const payment = document.getElementById('payment');
+const creditCard = document.getElementById('credit-card');
+const paypal = document.getElementById('paypal');
+const bitcoin = document.getElementById('bitcoin');
 const selectedPayment = payment.firstElementChild.nextElementSibling;
 //console.log(payment);
 //console.log(creditCard);
 //console.log(paypal);
 //console.log(bitcoin);
 
-paypal.style.display = ‘none’;
-bitcoin.style.display = ‘none’;
+paypal.style.display = 'none';
+bitcoin.style.display = 'none';
 selectedPayment.selected = true;
 //console.log(selectedPayment);
 
 //the payment method selected will show and the others will be hidden
-payment.addEventListener(‘change’, (e) => {
-  if (e.target.value === ‘paypal’) {
-    paypal.style.display = ‘block’;
-    bitcoin.style.display = ‘none’;
-    creditCard.style.display = ‘none’;
+payment.addEventListener('change', (e) => {
+  if (e.target.value === 'paypal') {
+    paypal.style.display = 'block';
+    bitcoin.style.display = 'none';
+    creditCard.style.display = 'none';
   }
-  else if (e.target.value === ‘bitcoin’) {
-    bitcoin.style.display = ‘block’;
-    paypal.style.display = ‘none’;
-    creditCard.style.display = ‘none’;
+  else if (e.target.value === 'bitcoin') {
+    bitcoin.style.display = 'block';
+    paypal.style.display = 'none';
+    creditCard.style.display = 'none';
   }
   else {
-    paypal.style.display = ‘none’;
-    bitcoin.style.display = ‘none’;
-    creditCard.style.display = ‘block’;
+    paypal.style.display = 'none';
+    bitcoin.style.display = 'none';
+    creditCard.style.display = 'block';
   }
 });
 
 
 //form validation section
-const name = document.getElementById(‘name’);
-const email = document.getElementById(‘email’);
-const ccNumber = document.getElementById(‘cc-num’);
-const zipcode = document.getElementById(‘zip’);
-const cvv = document.getElementById(‘cvv’);
-const form = document.querySelector(‘form’);
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const ccNumber = document.getElementById('cc-num');
+const zipcode = document.getElementById('zip');
+const cvv = document.getElementById('cvv');
+const form = document.querySelector('form');
 
 //passing an element through the function to see if it passes or fails
 function passedValidation(element) {
   const formField = element.parentElement;
-  formField.classList.add(‘valid’);
-  formField.classList.remove(‘not-valid’);
-  formField.lastElementChild.style.display = ‘none’;
+  formField.classList.add('valid');
+  formField.classList.remove('not-valid');
+  formField.lastElementChild.style.display = 'none';
 }
 
 function failedValidation(element) {
   const formField = element.parentElement;
-  formField.classList.add(‘not-valid’);
-  formField.classList.remove(‘valid’);
-  formField.lastElementChild.style.display = ‘block’;
+  formField.classList.add('not-valid');
+  formField.classList.remove('valid');
+  formField.lastElementChild.style.display = 'block';
 }
 
 
@@ -197,7 +197,7 @@ return attendeeCvv;
 }
 
 //Use the “form” variable to listen for the submit event
-form.addEventListener( ‘submit’, e => {
+form.addEventListener( 'submit', e => {
 validName();
 validEmail();
 validActivity();
